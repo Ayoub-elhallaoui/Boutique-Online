8b57fbc0021c4a342a5199180cfc40ea1ff5707a
r00tMG <meissagningue7@gmail.com>
2024-05-10 17:05:11 +0100

    update index.php and products.csv

---
+++ corbeille.php
@@ -0,0 +1,263 @@
+<!-- <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales"> 
+
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="">
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Piqué Biker Jacket</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$67.24</h5>
+                            <div class="product__color__select">
+                                <label for="pc-4">
+                                    <input type="radio" id="pc-4">
+                                </label>
+                                <label class="active black" for="pc-5">
+                                    <input type="radio" id="pc-5">
+                                </label>
+                                <label class="grey" for="pc-6">
+                                    <input type="radio" id="pc-6">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item sale">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
+                            <span class="label">Sale</span>
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Multi-pocket Chest Bag</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$43.48</h5>
+                            <div class="product__color__select">
+                                <label for="pc-7">
+                                    <input type="radio" id="pc-7">
+                                </label>
+                                <label class="active black" for="pc-8">
+                                    <input type="radio" id="pc-8">
+                                </label>
+                                <label class="grey" for="pc-9">
+                                    <input type="radio" id="pc-9">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Diagonal Textured Cap</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$60.9</h5>
+                            <div class="product__color__select">
+                                <label for="pc-10">
+                                    <input type="radio" id="pc-10">
+                                </label>
+                                <label class="active black" for="pc-11">
+                                    <input type="radio" id="pc-11">
+                                </label>
+                                <label class="grey" for="pc-12">
+                                    <input type="radio" id="pc-12">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-5.jpg">
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Lether Backpack</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$31.37</h5>
+                            <div class="product__color__select">
+                                <label for="pc-13">
+                                    <input type="radio" id="pc-13">
+                                </label>
+                                <label class="active black" for="pc-14">
+                                    <input type="radio" id="pc-14">
+                                </label>
+                                <label class="grey" for="pc-15">
+                                    <input type="radio" id="pc-15">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item sale">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-6.jpg">
+                            <span class="label">Sale</span>
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Ankle Boots</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$98.49</h5>
+                            <div class="product__color__select">
+                                <label for="pc-16">
+                                    <input type="radio" id="pc-16">
+                                </label>
+                                <label class="active black" for="pc-17">
+                                    <input type="radio" id="pc-17">
+                                </label>
+                                <label class="grey" for="pc-18">
+                                    <input type="radio" id="pc-18">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-7.jpg">
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>T-shirt Contrast Pocket</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$49.66</h5>
+                            <div class="product__color__select">
+                                <label for="pc-19">
+                                    <input type="radio" id="pc-19">
+                                </label>
+                                <label class="active black" for="pc-20">
+                                    <input type="radio" id="pc-20">
+                                </label>
+                                <label class="grey" for="pc-21">
+                                    <input type="radio" id="pc-21">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="img/product/product-8.jpg">
+                            <ul class="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                            </ul>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Basic Flowing Scarf</h6>
+                            <a href="#" class="add-cart">+ Add To Cart</a>
+                            <div class="rating">
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                                <i class="fa fa-star-o"></i>
+                            </div>
+                            <h5>$26.28</h5>
+                            <div class="product__color__select">
+                                <label for="pc-22">
+                                    <input type="radio" id="pc-22">
+                                </label>
+                                <label class="active black" for="pc-23">
+                                    <input type="radio" id="pc-23">
+                                </label>
+                                <label class="grey" for="pc-24">
+                                    <input type="radio" id="pc-24">
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+
+
+
+img/products/wool-sweater.jpg,Merino Wool Sweater,"Stay warm and stylish with this merino wool sweater. Made from premium wool, it's soft, breathable, and perfect for layering.",Men,Tops,S/M/L/XL,89.99,35
+img/products/aviator-sunglasses.jpg,Aviator Sunglasses,"These classic aviator sunglasses are a timeless accessory. With polarized lenses and a durable metal frame, they offer both style and protection from the sun.",Men,Accessories,One Size,49.99,65
+img/products/oxford-shoes.jpg,Leather Oxford Shoes,"These handsome leather oxford shoes are perfect for any formal or professional occasion. With a sleek design and premium leather construction, they'll elevate any outfit.",Men,Footwear,8-12,119.99,25
+img/products/canvas-backpack.jpg,Canvas Backpack,"This rugged canvas backpack is perfect for everyday use or outdoor adventures. With multiple compartments and a padded laptop sleeve, it's both stylish and functional.",Men,Bags,One Size,59.99,40
+img/products/cotton-polo.jpg,Cotton Pique Polo Shirt,"This classic cotton pique polo shirt is a wardrobe essential. Made from soft, breathable cotton, it features a ribbed collar and cuffs for a polished look.",Men,Tops,S/M/L/XL,39.99,50
+img/products/suede-boots.jpg,Suede Chelsea Boots,"These suede chelsea boots are a stylish and versatile addition to any shoe collection. With a sleek design and premium construction, they're perfect for dressing up or down.",Men,Footwear,8-12,129.99,30
+img/products/wool-blazer.jpg,Wool Blazer,"This classic wool blazer is a must-have for any modern man's wardrobe. With a tailored fit and premium construction, it's perfect for both casual and formal occasions.",Men,Outerwear,36-46,199.99,25
+img/products/leather-wallet.jpg,Bifold Leather Wallet,"Crafted from premium full-grain leather, this bifold wallet is both durable and stylish. With multiple card slots and a cash compartment, it's perfect for everyday carry.",Men,Accessories,One Size,49.99,60
+img/products/canvas-tote.jpg,Canvas Tote Bag,"This versatile canvas tote bag is perfect for everyday use or travel. With a spacious main compartment and sturdy handles, it's both functional and stylish.",Men,Bags,One Size,29.99,50
+img/products/trucker-jacket.jpg,Trucker Denim Jacket,"This classic trucker jacket is a wardrobe staple for any modern man. Made from premium denim, it features a button-front design and adjustable waist tabs.",Men,Outerwear,S/M/L/XL,99.99,40
+img/products/chino-shorts.jpg,Stretch Chino Shorts,"These versatile chino shorts are perfect for warm weather. Made from a stretchy cotton-blend fabric, they offer a comfortable and stylish fit.",Men,Bottoms,28-38,49.99,45
+img/products/leather-messenger.jpg,Leather Messenger Bag,"This handsome leather messenger bag is perfect for the modern professional. With plenty of compartments and a padded laptop sleeve, it's both stylish and functional.",Men,Bags,15" x 11" x 4",149.99,30
+img/products/cotton-cardigan.jpg,Cotton Cardigan Sweater,"This lightweight cotton cardigan is perfect for layering. With a classic button-front design and ribbed trim, it's both comfortable and stylish.",Men,Tops,S/M/L/XL,69.99,40
+img/products/leather-gloves.jpg,Leather Driving Gloves,"These premium leather driving gloves are a stylish and functional accessory. With a flexible construction and perforated detailing, they're perfect for any occasion.",Men,Accessories,S/M/L/XL,59.99,50
+img/products/flannel-shirt-2.jpg,Plaid Flannel Shirt,"This classic plaid flannel shirt is a wardrobe staple for any outdoorsman. Made from soft, brushed cotton, it's perfect for layering and outdoor adventures.",Men,Tops,S/M/L/XL,49.99,45
+img/products/slippers.jpg,Suede Slippers,"These luxurious suede slippers are perfect for lounging at home. With a soft, cushioned footbed and durable construction, they offer both comfort and style.",Men,Footwear,8-12,69.99,35
+img/products/chino-jacket.jpg,Cotton Chino Jacket,"This versatile cotton chino jacket is perfect for transitional weather. With a classic button-front design and multiple pockets, it's both stylish and functional.",Men,Outerwear,S/M/L/XL,89.99,40
+img/products/swim-trunks.jpg,Quick-Dry Swim Trunks,"These quick-dry swim trunks are perfect for any beach or pool day. With a comfortable elastic waistband and mesh lining, they offer both style and functionality.",Men,Bottoms,S/M/L/XL,39.99,50
+img/products/wool-socks.jpg,Merino Wool Socks,"These premium merino wool socks are perfect for any occasion. With a soft, breathable construction and reinforced heels and toes.",Men,Accessories,S/M/XL,19.99,75
