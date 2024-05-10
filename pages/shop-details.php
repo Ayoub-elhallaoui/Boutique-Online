76d9ff477e871c2206ed2304de1a68cff347c60d
r00tMG <meissagningue7@gmail.com>
2024-05-10 10:35:24 +0100

    create user.csv update *.php

--- pages/shop-details.php
+++ pages/shop-details.php
@@ -99,7 +99,7 @@
                 <div class="col-lg-6 col-md-6">
                     <nav class="header__menu mobile-menu">
                         <ul>
-                            <li><a href="./inex.php">Home</a></li>
+                            <li><a href="../inex.php">Home</a></li>
                             <li class="active"><a href="./shop.php">Shop</a></li>
                             <li><a href="#">Pages</a>
                                 <ul class="dropdown">
